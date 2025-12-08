03c366727562013599a082646e0ea13aaccea225
Shafaq Arefin Chowdhury <65824125+shafaqarefin@users.noreply.github.com>
2025-12-08 16:36:07 +0600

    Update snr_apply import to use snr_and_reverb

--- dataspeech/__init__.py
+++ dataspeech/__init__.py
@@ -1,5 +1,5 @@
 from .cpu_enrichments import rate_apply
 # from .gpu_enrichments import pitch_apply, snr_apply, squim_apply
-from .gpu_enrichments.snr import snr_apply
+from .gpu_enrichments.snr_and_reverb import snr_apply
 from .gpu_enrichments.squim import squim_apply
 # pitch_apply skipped due to torbi incompatibility
